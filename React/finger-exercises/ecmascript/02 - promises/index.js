14f3bf8f4605c70ebacba12ca3046d94c2a92c9a
EmmanuelWLX <emmanuel.estigarriba@wolox.com.ar>
2018-12-13 11:02:27 -0300

    Tests 11 and 12 passed. delay() functions well, and throws an error when the time is too long

--- React/finger-exercises/ecmascript/02 - promises/index.js
+++ React/finger-exercises/ecmascript/02 - promises/index.js
@@ -1,9 +1,17 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
-export function delay() {
-
+export function delay(timeMS) {
+  // return new Promise((resolve) => setTimeout(resolve, timeMS));
+  const tempIni = Date.now();
+  return new Promise((resolve, reject) => {
+    if (timeMS >= 4000) {
+      reject(new Error('This time is too much !'));
+    }
+    setInterval(() => {
+      resolve(Date.now() - tempIni);
+    }, timeMS);
+  });
 }
-
 export function asyncDelay() {
 
 }
