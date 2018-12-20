57f2f33e42074e88ffb2b34e02de5fa9457b03b9
EmmanuelWLX <emmanuel.estigarriba@wolox.com.ar>
2018-12-20 10:23:01 -0300

    Inspecting the starter code. Now is displayed an X when a Square is clicked.

--- React/tic-tac-toe/src/app/screens/Game/components/Square/index.js
+++ React/tic-tac-toe/src/app/screens/Game/components/Square/index.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import PropTypes from 'prop-types';
 
 import styles from './styles.module.scss';
 
-const Square = () => (
-  <button type="button" className={styles.square}>
-    {/* TODO */}
+/*
+const Square = ({value }) => (
+  <button type="button" className={styles.square} onClick={() => alert('click') }>
+    {value}
   </button>
 );
+*/
+
+class Square extends Component {
+  state = {
+    value: null
+  };
+
+  render() {
+    return (
+      <button
+        type="button"
+        className={styles.square}
+        onClick={() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
+      </button>
+    );
+  }
+}
+
+Square.propTypes = {
+  value: PropTypes.number
+};
 
 export default Square;
