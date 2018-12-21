54aa4430c54cc7b2187c065f36cf7ba9c3884078
EmmanuelWLX <emmanuel.estigarriba@wolox.com.ar>
2018-12-21 11:37:01 -0300

    Declaring a winner

--- React/tic-tac-toe/src/app/screens/Game/components/Board/index.js
+++ React/tic-tac-toe/src/app/screens/Game/components/Board/index.js
@@ -13,6 +13,9 @@ class Board extends Component {
   handleClick = (i) => () => {
     this.setState(prevState => {
       const squaresNew = [...prevState.squares];
+      if (calculateWinner(squaresNew) || squaresNew[i]) {
+        return;
+      }
       squaresNew[i] = prevState.xIsNext ? 'X' : 'O';
       return { squares: squaresNew, xIsNext: !prevState.xIsNext };
     });
@@ -28,7 +31,13 @@ class Board extends Component {
   }
 
   render() {
-    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    const winner = calculateWinner(this.state.squares);
+    let status = '';
+    if (winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    }
     return (
       <React.Fragment>
         <div className={styles.status}>{status}</div>
@@ -50,6 +59,26 @@ class Board extends Component {
       </React.Fragment>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default Board;
