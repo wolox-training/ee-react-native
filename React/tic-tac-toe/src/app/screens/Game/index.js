8e9fba037a5e23ede5e4245512efd2dfc92c3aef
EmmanuelWLX <emmanuel.estigarriba@wolox.com.ar>
2018-12-21 14:30:48 -0300

    Game finished. Throwback is real

--- React/tic-tac-toe/src/app/screens/Game/index.js
+++ React/tic-tac-toe/src/app/screens/Game/index.js
@@ -6,12 +6,13 @@ import Board from './components/Board';
 class Game extends Component {
   state = {
     history: [{ squares: Array(9).fill(null) }],
+    stepNumber: 0,
     xIsNext: true
   };
 
   handleClick = (i) => () => {
     this.setState(prevState => {
-      const history = [...prevState.history];
+      const history = prevState.history.slice(0, prevState.stepNumber + 1);
       const current = history[history.length - 1];
       const squaresNew = [...current.squares];
       if (calculateWinner(squaresNew) || squaresNew[i]) {
@@ -22,15 +23,34 @@ class Game extends Component {
         history: history.concat([{
           squares: squaresNew,
         }]),
+        stepNumber: history.length,
         xIsNext: !prevState.xIsNext };
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0
     });
   }
 
   render () {
     const history = [...this.state.history];
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
     const ended = isBoardFullfilled(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move
+        ? `Go to move #${move}`
+        : 'Go to game start';
+      return (
+        <li key={move}>
+          <button type="button" onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
 
     let status = '';
     if (winner) {
@@ -51,7 +71,7 @@ class Game extends Component {
         </div>
         <div className={styles.gameInfo}>
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
