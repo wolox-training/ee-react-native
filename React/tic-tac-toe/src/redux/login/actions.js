6069da49b9ee3ecd4e1bc0176bc774aef17c194c
EmmanuelWLX <emmanuel.estigarriba@wolox.com.ar>
2019-01-28 11:47:22 -0300

    Deleted console logs

--- React/tic-tac-toe/src/redux/login/actions.js
+++ React/tic-tac-toe/src/redux/login/actions.js
@@ -18,32 +18,26 @@ const actionCreators = {
   login: (user) => async dispatch => {
     dispatch({ type: actions.LOGIN_REQUEST });
     const response = await loginService({ email: user.email });
-    console.warn(response.data);
-    console.warn(response.data[0]);
     if (response.ok) {
       if (response.data.length > 0) {
         if (response.data[0].password === user.password) {
-          console.warn('EXITOO');
           dispatch({
             type: actions.LOGIN_SUCCESS,
             payload: response.data
           });
         } else {
-          console.warn('MALA CONTRASENIA');
           dispatch({
             type: actions.LOGIN_FAILURE,
             payload: 'Password is incorrect'
           });
         }
       } else {
-        console.warn('EMAIL NO ESSSSISSSTE');
         dispatch({
           type: actions.LOGIN_FAILURE,
           payload: 'Email not registered yet'
         });
       }
     } else {
-      console.warn('NO SE PUDO CONECTAR');
       dispatch({
         type: actions.LOGIN_FAILURE,
         payload: 'Connection error'
