f539c4ac4ffc39c9ea01c8b9a2d4ae851cfd2937
EmmanuelWLX <emmanuel.estigarriba@wolox.com.ar>
2019-01-29 10:54:14 -0300

    Some fixes

--- React/tic-tac-toe/src/redux/login/actions.js
+++ React/tic-tac-toe/src/redux/login/actions.js
@@ -3,9 +3,7 @@ import { stringArrayToObject } from '../../utils/stringUtils';
 export const actions = stringArrayToObject(
   [
     'GET_LOGGED_USER',
-    'LOGIN_REQUEST',
-    'LOGIN_SUCCESS',
-    'LOGIN_FAILURE'
+    'LOGIN_SUCCESS'
   ],
   '@@LOGIN'
 );
@@ -14,7 +12,7 @@ const actionCreators = {
   getLoggedUser: () => ({
     type: actions.GET_LOGGED_USER
   }),
-  login: (status) => dispatch => {
+  login: status => dispatch => {
     dispatch({ type: actions.LOGIN_SUCCESS, payload: status });
   }
 };
