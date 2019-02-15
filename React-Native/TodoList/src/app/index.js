869b1370f39f19aadec860bea133769f993a05d0
EmmanuelWLX <emmanuel.estigarriba@wolox.com.ar>
2019-02-15 15:31:37 -0300

    Last fixes

--- React-Native/TodoList/src/app/index.js
+++ React-Native/TodoList/src/app/index.js
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 import store from '../redux/store';
 import AppContainer from './AppContainer';
 
-const App = function App () {
+function App () {
     return(
       <Provider store={store}>
         <AppContainer/>
